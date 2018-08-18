c7c5e9b53a3ad2c83325880d39c977b2c32189c1
Zhandos <saginov-2001@mail.ru>
2018-08-18 20:25:44 +0300

    выфвфы

---
+++ header.php
@@ -0,0 +1,14 @@
+<div class="header">
+	<div class="logo">
+		<a href="index.php"><div class="logo-container">
+		</div>
+		<span class="logo-title">KAZAS</span><span class="logo-subtitle">рули своей жизнью</span></a>
+	</div>
+	<ul class="navbar">
+		<li class="nav-item"><a href="" class="nav-link"><i class="fas fa-car"><span> Автошколы</span></i></a></li>
+		<li class="nav-item"><a href="" class="nav-link"><i class="fas fa-book"><span> Подготовка</span></i></a></li>
+		<li class="nav-item"><a href="" class="nav-link"><i class="fas fa-address-book"><span> Контакты</span></i></a></li>
+		<li class="nav-item"><a href="" class="nav-link"><i class="fas fa-sign-in-alt"><span> Войти</span></i></a></li>
+
+	</ul>
+</div>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Autoschool</title>
+
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="stylesheet" href="css/header.css">
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa|Faster+One|Montserrat+Alternates" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/swiper.css">
+	
+</head>
+<body>
+	<?php include 'header.php'; ?>
+	<div class="swiper-container">
+    <!-- Additional required wrapper -->
+    	<div class="swiper-wrapper">
+        <!-- Slides -->
+        	<div class="swiper-slide">
+        		<img class="slide-image" src="slider/slide1.jpg" alt=""><div class="slider-label"><span class="label">Подготовка к экзаменам онлайн</span><span class="sublabel">Мы приготовили для вас специальные видеоуроки и тестовые задания, которые помогут вам успешно сдать ,как теоритическую, так и практическую часть экзамена на получение прав разных классификаций</span><div onclick="alert('123');" class="slider-button"><span>Продолжить</span></div></div>
+				
+
+        	</div>
+        	<div class="swiper-slide"><img class="slide-image" src="slider/slide2.jpg" alt=""><div class="slider-label"><span class="label">Зарекомендуй себя как профессионального инструктора</span><span class="sublabel">Зарегестрийруйтесь и зарекомендуйте себя как профессионала своего дела, расскажите о себе и докажите, что именно вы можете научить любого желающего вне зависимости от его опыта</span><div onclick="alert('123');" class="slider-button"><span>Подробнее</span></div></div></div>
+        	<div class="swiper-slide">Slide 3</div>
+        
+   		</div>
+    <!-- If we need pagination -->
+    	<div class="swiper-pagination"></div>
+
+    <!-- If we need navigation buttons -->
+    	<div class="swiper-button-prev"></div>
+    	<div class="swiper-button-next"></div>
+    </div>
+				<div>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum soluta adipisci reiciendis neque quae maiores sit temporibus, laudantium, facere doloremque, non modi voluptate possimus quisquam repudiandae debitis voluptatem veritatis ad!</div>
+				<div>Molestiae nobis, veritatis ut. Dolore iusto labore facere ex, alias numquam perspiciatis itaque, quia possimus aliquid odit reprehenderit voluptatibus beatae esse asperiores odio totam sequi? Reiciendis minima totam architecto corrupti.</div>
+				<div>Consequatur temporibus id rerum cupiditate debitis sequi dolore maxime, veniam, dolores vel asperiores quam eius nesciunt iusto nostrum quis, eum velit sit doloremque cum autem exercitationem laborum, consectetur? Deleniti, eveniet.</div>
+				<div>Ea dolorem asperiores optio, voluptates autem quibusdam voluptatibus officia perspiciatis possimus ex delectus deserunt aliquid voluptas iste adipisci deleniti maiores itaque laboriosam quidem, rem odit eos laborum laudantium quos minima!</div>
+				<div>Nisi libero dicta facere, earum. Exercitationem, fugiat! Eos debitis inventore asperiores provident suscipit eum laborum tenetur consequatur eaque at eligendi reiciendis nemo, ullam corrupti rerum praesentium aliquam, velit saepe animi.</div>
+				<div>Vitae necessitatibus unde blanditiis nesciunt possimus eum veniam nulla rerum eligendi ducimus amet, veritatis ex nostrum error deleniti! Officiis atque, libero beatae amet neque facilis sapiente natus vitae ea cum?</div>
+				<div>Odit sapiente, beatae. Molestiae aliquid vel praesentium perspiciatis obcaecati tempore suscipit veniam, animi, dolor minima, ea quibusdam. Veritatis consectetur ullam magni incidunt eveniet voluptas assumenda ratione earum quos in, quibusdam!</div>
+				<div>Aperiam voluptatibus modi aliquid hic saepe, libero excepturi sunt enim cum possimus. Expedita, esse. Tempora dicta nesciunt officiis accusamus, reprehenderit excepturi voluptates non laudantium quia quos consequuntur minus, fugiat omnis?</div>
+				<div>Similique, reprehenderit, culpa neque at mollitia consectetur cumque deleniti tempore recusandae sunt in hic iste laudantium inventore tempora fugiat eius! Voluptates dolorem sapiente nobis, accusantium ipsa mollitia consequatur corporis assumenda.</div>
+				<div>Fugit laborum, deleniti necessitatibus vero ducimus quae corporis perferendis perspiciatis, sint incidunt esse nam culpa, maxime voluptates ad itaque consequuntur deserunt totam quibusdam. Officia, dolor repellendus labore maxime rem consequuntur?</div>
+				<div>Tempora placeat ratione blanditiis commodi quia quam fugit, quo earum magnam, expedita, ipsa. Laudantium, incidunt, nihil. Eius fuga officia molestiae optio blanditiis, sed expedita earum minus accusantium, voluptatum ad beatae.</div>
+				<div>Voluptatibus quia accusantium, officiis nam, deserunt similique nemo ducimus nostrum sed veniam in praesentium natus facilis facere dolor. Reprehenderit nostrum tempora temporibus, fugit nesciunt quo saepe perferendis laudantium maiores tempore!</div>
+				<div>Maxime reiciendis, voluptatibus architecto debitis inventore officia voluptates molestiae veniam. Impedit maxime numquam odio laudantium sed veniam, repellendus magnam suscipit totam dolore id est voluptatem vel sapiente vitae eius quis.</div>
+				<div>Debitis, perspiciatis, voluptate! Deserunt vero ad aperiam sapiente animi aspernatur cumque at, fuga harum molestiae explicabo esse reprehenderit, in voluptatum quis officia nam. Quaerat velit, recusandae laudantium a blanditiis omnis.</div>
+				<div>Expedita non ullam, consequatur. Debitis assumenda harum, sed dolore laudantium cum, esse numquam dolor! Mollitia eveniet, sed. Accusantium corporis saepe ipsum dolor tenetur cumque, quo voluptatem cum, et libero alias!</div>
+				<div>Beatae consequatur, harum quis omnis at cupiditate incidunt atque quidem ratione ut iusto ducimus dolore possimus cumque itaque magni repellendus quae id, provident, officia reprehenderit eius facere velit! Vero, et!</div>
+				<div>Debitis laudantium quo, animi odit autem aliquid nisi repudiandae quos, recusandae ratione alias quae tenetur molestiae commodi sapiente saepe esse rerum rem magni earum. Consequuntur vitae suscipit, facilis nobis laboriosam.</div>
+				<div>Ullam nihil, quia maxime rem aspernatur in voluptatum dolor quam culpa quaerat et tempora, quidem praesentium odio tenetur facilis quisquam laboriosam mollitia, excepturi ipsum laudantium nobis sint eum voluptates. Impedit.</div>
+				<div>Mollitia explicabo, molestiae! Sit aperiam tenetur temporibus similique corrupti, ea possimus, excepturi modi asperiores! Tempore eligendi dignissimos, ea quam, optio nulla nisi numquam animi ipsum id officia perspiciatis possimus necessitatibus.</div>
+				<div>Saepe voluptatibus totam deserunt laborum quae, hic vero, maxime dignissimos minus quo odio odit ullam alias eius nemo! Aliquam libero maiores placeat, ducimus architecto eligendi porro voluptas ab beatae quibusdam!</div>
+
+</body>
+<script
+  src="http://code.jquery.com/jquery-3.3.1.slim.js"
+  integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+  crossorigin="anonymous"></script>
+  <script src="js/swiper.js"></script>
+
+<script>
+    $(document).ready(function () {
+    	var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 1000,
+    loop: true,
+    autoplay: {
+    delay: 40000,
+  	},
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+    // And if we need scrollbar
+    
+  });
+    });
+  </script>
+  <script>
+  
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() > 50) { $('.header').addClass('fixed');   }
+          else { $('.header').removeClass('fixed');  }
+    });     
+  
+</script>
+</html>
