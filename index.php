21ddbbab04235048859089e99e5113061cd5a984
Zhandos <saginov-2001@mail.ru>
2018-08-18 22:53:47 +0300

    sadasdasd

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 	<link rel="stylesheet" href="css/css.css">
 	<link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/search.css">
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa|Faster+One|Montserrat+Alternates" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/swiper.css">
@@ -34,27 +35,34 @@
     	<div class="swiper-button-prev"></div>
     	<div class="swiper-button-next"></div>
     </div>
-				<div>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum soluta adipisci reiciendis neque quae maiores sit temporibus, laudantium, facere doloremque, non modi voluptate possimus quisquam repudiandae debitis voluptatem veritatis ad!</div>
-				<div>Molestiae nobis, veritatis ut. Dolore iusto labore facere ex, alias numquam perspiciatis itaque, quia possimus aliquid odit reprehenderit voluptatibus beatae esse asperiores odio totam sequi? Reiciendis minima totam architecto corrupti.</div>
-				<div>Consequatur temporibus id rerum cupiditate debitis sequi dolore maxime, veniam, dolores vel asperiores quam eius nesciunt iusto nostrum quis, eum velit sit doloremque cum autem exercitationem laborum, consectetur? Deleniti, eveniet.</div>
-				<div>Ea dolorem asperiores optio, voluptates autem quibusdam voluptatibus officia perspiciatis possimus ex delectus deserunt aliquid voluptas iste adipisci deleniti maiores itaque laboriosam quidem, rem odit eos laborum laudantium quos minima!</div>
-				<div>Nisi libero dicta facere, earum. Exercitationem, fugiat! Eos debitis inventore asperiores provident suscipit eum laborum tenetur consequatur eaque at eligendi reiciendis nemo, ullam corrupti rerum praesentium aliquam, velit saepe animi.</div>
-				<div>Vitae necessitatibus unde blanditiis nesciunt possimus eum veniam nulla rerum eligendi ducimus amet, veritatis ex nostrum error deleniti! Officiis atque, libero beatae amet neque facilis sapiente natus vitae ea cum?</div>
-				<div>Odit sapiente, beatae. Molestiae aliquid vel praesentium perspiciatis obcaecati tempore suscipit veniam, animi, dolor minima, ea quibusdam. Veritatis consectetur ullam magni incidunt eveniet voluptas assumenda ratione earum quos in, quibusdam!</div>
-				<div>Aperiam voluptatibus modi aliquid hic saepe, libero excepturi sunt enim cum possimus. Expedita, esse. Tempora dicta nesciunt officiis accusamus, reprehenderit excepturi voluptates non laudantium quia quos consequuntur minus, fugiat omnis?</div>
-				<div>Similique, reprehenderit, culpa neque at mollitia consectetur cumque deleniti tempore recusandae sunt in hic iste laudantium inventore tempora fugiat eius! Voluptates dolorem sapiente nobis, accusantium ipsa mollitia consequatur corporis assumenda.</div>
-				<div>Fugit laborum, deleniti necessitatibus vero ducimus quae corporis perferendis perspiciatis, sint incidunt esse nam culpa, maxime voluptates ad itaque consequuntur deserunt totam quibusdam. Officia, dolor repellendus labore maxime rem consequuntur?</div>
-				<div>Tempora placeat ratione blanditiis commodi quia quam fugit, quo earum magnam, expedita, ipsa. Laudantium, incidunt, nihil. Eius fuga officia molestiae optio blanditiis, sed expedita earum minus accusantium, voluptatum ad beatae.</div>
-				<div>Voluptatibus quia accusantium, officiis nam, deserunt similique nemo ducimus nostrum sed veniam in praesentium natus facilis facere dolor. Reprehenderit nostrum tempora temporibus, fugit nesciunt quo saepe perferendis laudantium maiores tempore!</div>
-				<div>Maxime reiciendis, voluptatibus architecto debitis inventore officia voluptates molestiae veniam. Impedit maxime numquam odio laudantium sed veniam, repellendus magnam suscipit totam dolore id est voluptatem vel sapiente vitae eius quis.</div>
-				<div>Debitis, perspiciatis, voluptate! Deserunt vero ad aperiam sapiente animi aspernatur cumque at, fuga harum molestiae explicabo esse reprehenderit, in voluptatum quis officia nam. Quaerat velit, recusandae laudantium a blanditiis omnis.</div>
-				<div>Expedita non ullam, consequatur. Debitis assumenda harum, sed dolore laudantium cum, esse numquam dolor! Mollitia eveniet, sed. Accusantium corporis saepe ipsum dolor tenetur cumque, quo voluptatem cum, et libero alias!</div>
-				<div>Beatae consequatur, harum quis omnis at cupiditate incidunt atque quidem ratione ut iusto ducimus dolore possimus cumque itaque magni repellendus quae id, provident, officia reprehenderit eius facere velit! Vero, et!</div>
-				<div>Debitis laudantium quo, animi odit autem aliquid nisi repudiandae quos, recusandae ratione alias quae tenetur molestiae commodi sapiente saepe esse rerum rem magni earum. Consequuntur vitae suscipit, facilis nobis laboriosam.</div>
-				<div>Ullam nihil, quia maxime rem aspernatur in voluptatum dolor quam culpa quaerat et tempora, quidem praesentium odio tenetur facilis quisquam laboriosam mollitia, excepturi ipsum laudantium nobis sint eum voluptates. Impedit.</div>
-				<div>Mollitia explicabo, molestiae! Sit aperiam tenetur temporibus similique corrupti, ea possimus, excepturi modi asperiores! Tempore eligendi dignissimos, ea quam, optio nulla nisi numquam animi ipsum id officia perspiciatis possimus necessitatibus.</div>
-				<div>Saepe voluptatibus totam deserunt laborum quae, hic vero, maxime dignissimos minus quo odio odit ullam alias eius nemo! Aliquam libero maiores placeat, ducimus architecto eligendi porro voluptas ab beatae quibusdam!</div>
-
+	<div class="search-nav">
+		<span class="city-c">Выберете город</span>
+		<div class="filter">
+			<select name="city" value="Выберете город" id="filter">
+				<option selected="true" value="all">Все</option>
+				<option  value="Астана">Астана</option>
+				<option value="Алматы">Алматы</option>
+				<option value="Уральск">Уральск</option>
+				<option value="Шымкент">Шымкент</option>
+				<option value="Актау">Актау</option>
+			</select>
+		</div>
+		<div class="search-button"><div style="position: relative; width: 100%; height: 100%;"><span class="search-label"><i class="fa fa-search"></i> Поиск</span> </div>
+	</div>
+	</div>
+	<div>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eos cum est repellat maiores assumenda similique odit perferendis accusantium ut aliquid a debitis, dicta molestiae doloribus atque adipisci, quos amet!</div>
+	<div>Corrupti laboriosam, voluptates vitae. Voluptate, cupiditate, placeat perspiciatis illo, alias est possimus ut accusamus molestias tenetur ipsam amet in! Quos nulla saepe odit quod exercitationem, aliquid error numquam neque voluptatibus!</div>
+	<div>Esse sed ipsa quod expedita laudantium architecto illum quas voluptatibus, aliquid et quae unde perspiciatis suscipit illo maiores fugit aspernatur similique eum pariatur dolorum quasi corrupti minima. Minus, molestias illo?</div>
+	<div>Expedita natus doloribus eveniet! At corporis id exercitationem reiciendis ex tenetur, vitae eos odit voluptatibus expedita! Quisquam asperiores incidunt dolorum at tenetur, distinctio quos similique natus, tempora facere provident, inventore.</div>
+	<div>Natus ipsum aliquam, nostrum laboriosam sapiente quae alias, molestiae tempore accusamus cum voluptatem veritatis architecto doloremque magnam facilis eius explicabo vel dolor. Harum fugit, omnis nisi et aliquid dolor eaque!</div>
+	<div>	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla qui deserunt adipisci aperiam distinctio repellendus, veniam aspernatur id! Inventore molestiae, laborum harum eum. Harum veritatis et officiis vel. Illo, modi!</div>
+	<div>Doloribus doloremque rem quia numquam cupiditate facilis illum nostrum eveniet deleniti vitae cum a at dolorum assumenda voluptate dolorem eum, corporis blanditiis consectetur vel nobis? Minima, laborum suscipit? Totam, ullam.</div>
+	<div>Maiores eum animi veniam tempore nemo. Soluta illo, inventore debitis optio. Dicta ipsa ducimus delectus tempore sint obcaecati mollitia dolores, eligendi deserunt vel, aliquam aut totam numquam est, voluptatibus modi.</div>
+	<div>Architecto at placeat recusandae perspiciatis necessitatibus. Minima, architecto esse odit dolor voluptatem ab! Temporibus earum quos esse explicabo accusantium, fuga rerum vitae, aliquam dicta cupiditate tempore maiores itaque sit velit.</div>
+	<div>Cum temporibus illo ex ipsum cupiditate asperiores repellendus vel voluptas in iste vitae, placeat esse earum ab inventore facere. Similique, laboriosam eligendi earum unde rerum deserunt, doloribus nihil aliquid non.</div>
+	<div>Eius, doloremque. Modi unde, libero harum dignissimos, accusantium doloribus. Ducimus officiis excepturi id cupiditate possimus quae, distinctio consequatur illo. Perspiciatis harum voluptas perferendis debitis quibusdam non quasi doloremque atque culpa.</div>
+	<div>Ipsam inventore eveniet recusandae dolorum magnam laudantium, ut ratione voluptates cupiditate, repellendus, rem atque amet. Id ab quia ad voluptatum est animi! Quibusdam debitis, ab praesentium beatae illum est atque.</div>
+	<div>Odio voluptate maxime nobis doloribus beatae quaerat ipsam vel recusandae facere eos cumque adipisci deleniti facilis omnis modi enim quis cum corrupti numquam corporis, fuga sint ratione! Fugiat, nisi quis.</div>
 </body>
 <script
   src="http://code.jquery.com/jquery-3.3.1.slim.js"
