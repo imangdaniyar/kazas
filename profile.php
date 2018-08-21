7a4b6c745c49b945ae2f15ae2b094313cefe081c
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-08-21 15:40:35 +0500

    profile

---
+++ profile.php
@@ -0,0 +1,61 @@
+<?php include 'header2.php' ?>
+<div class="container">
+  <div class="u-profile">
+  	<div class="p-left">
+  		<div class="p-title">
+  			<span>Профиль - О себе</span>
+  		</div>
+  		<div class="p-card">
+  			<div class="p-personal">
+  				<div class="p-info" id="p-name">
+  				<i class="fa fa-user"></i> Имя
+	  			</div>
+
+	  			<div class="">Daniyar</div>
+	  			
+	  			<div class="p-info" id="p-sname">
+	  				<i class="fa fa-users"></i> Фамилия
+	  			</div>
+	  			
+	  			<div class="">Imangaliyev</div>
+
+	  			<div class="p-info" id="p-car">
+	  				<i class="fa fa-car"></i> Машина
+	  			</div>
+	  			
+	  			<div class="">Есть</div>
+	  			
+	  			<div class="p-info" id="p-exp">
+	  				<i class="fa fa-address-card"></i> Стаж вождения
+	  			</div>
+	  			
+	  			<div class="">18 лет</div>
+
+	  			<div class="p-info" id="p-exp">
+	  				<i class="fa fa-mobile"></i> Телефон
+	  			</div>
+	  			
+	  			<div class="">+7(777)777-77-77</div>
+	  			
+	  			<div class="p-info" id="p-info">
+	  				<i class="fa fa-caret-square-down"></i> Подробно
+	  			</div>	
+	  			
+	  			<div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque fugit .</div>
+  			</div>
+  			<div class="p-photo">
+  				<img src="slider/slide3.jpg" alt="" style="height:100%; width: 100%;">
+  			</div>
+  			
+  		</div>
+  	</div>
+  	<div class="p-right">
+  		
+  	</div>
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
+</body>
+<script src="js/js.js"></script>
+<script src="js/brain.js"></script>
