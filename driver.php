5e443d6de40a046c2b7aa0b1908a300cf23db6c1
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-08-23 01:28:08 +0500

    132

---
+++ driver.php
@@ -0,0 +1,86 @@
+<?php include 'header2.php' ?>
+<div class="container">
+  <div class="d-profile">
+  	<div class="d-left">
+  		<div class="d-ph">
+  			<img src="images/avatar.jpg" alt="" style="width: 100%;height: 100%;">
+  			<div class="d-ph-name">Daniyar Imangaliyev</div>	
+  		</div>
+  		
+  	</div>
+  	<div class="d-right">
+  		<div class="d-title">
+  			<span>Инструктор по вождению Имангалиев Данияр</span>
+  		</div>
+  		<div class="d-card">
+  			<div class="d-personal">
+  				<div class="d-info" id="d-name">
+  				<i class="fa fa-user"></i> Имя
+	  			</div>
+
+	  			<div class=""></div>
+	  			
+	  			<div class="d-info" id="d-sname">
+	  				<i class="fa fa-users"></i> Фамилия
+	  			</div>
+	  			
+	  			<div class=""></div>
+
+	  			<div class="d-info" id="d-car">
+	  				<i class="fa fa-car"></i> Машина
+	  			</div>
+	  			
+	  			<div class="">
+	  				
+					</div>	  				 
+	  			
+	  			<div class="d-info" id="d-exp">
+	  				<i class="fa fa-address-card"></i> Стаж вождения
+	  			</div>
+	  			
+	  			<div class="">
+	  				</div>
+
+	  			<div class="d-info" id="d-exp">
+	  				<i class="fa fa-mobile"></i> Телефон
+	  			</div>
+	  			
+	  			<div class="">+7(777)777-77-77</div>
+	  			
+	  			<div class="d-info" id="d-info">
+	  				<i class="fa fa-envelope"></i> E-mail
+	  			</div>	
+	  			
+	  			<div class=""></div>
+  			</div>
+  			<div class="personal-podrobno">
+  				<div class="podrobno">
+  					<i class="fa fa-envelope"></i> Подробно
+  				</div>
+  				<div class="podrobno-info">
+				Преподаю практические уроки вождения на своём(коробка автомат) либо вашем автомобиле.</div>
+  			</div>
+  			<div class="personal-podrobno">
+  				<div class="podrobno">Уроки</div>
+  				<div class="lessons">
+	  				<div class="l-chapter">
+	  					Вступление
+		  				<div class="lesson"><a href="#">Вождение за 1 час</a></div>
+		  				<div class="lesson"><a href="#">Как не врезаться об первый столб</a></div>
+		  				<div class="lesson"><a href="#">Саке Баке Маке или как никогда не получать штрафы</a></div>
+		  				<div class="lesson"><a href="#">Мастер-класс: вождение без мата</a></div>	
+	  				</div>
+  				</div>
+  			</div>
+  			
+  			
+  		</div>
+  	</div>
+  	
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
+</body>
+<script src="js/js.js"></script>
+<script src="js/brain.js"></script>
