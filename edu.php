fa1a8bab5a44cc8e22db14e6ddd34b99d54d46c6
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-08-25 18:33:01 +0500

    123

--- edu.php
+++ edu.php
@@ -1,8 +1,23 @@
 <?php include 'header2.php' ?>
 
+<div class="edu-nav">
+   <div class="e-choise">
+      <div class="pay active"><span>Платно</span></div>
+      <div class="free"><span>Бесплатно</span></div>
+    </div>
+    <div class="lessons" style="">
+    <ul class="navbar" style="text-align: center;">
+      <li class="nav-item" style="padding: 0.5vw;">Теоритический материал</li>
+      <li class="nav-item" style="padding: 0.5vw;">Видеоуроки</li>
+      <li class="nav-item" style="padding: 0.5vw;">Тесты</li>
+    </ul>
+  </div> 
+  </div>
 
 <div class="container nm">
 	
+  
+
 </div>
 
 <?php include 'footer.php' ?>
