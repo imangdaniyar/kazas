60f9636cc308ed1a0b7297919b2c3fe94a7f1385
Daniyar Imangaliyev <imangdaniyar@gmail.com>
2018-08-27 20:40:07 +0500

    123

--- v-l.php
+++ v-l.php
@@ -1,4 +1,10 @@
 <?php include 'header2.php' ?>
+<?php if(isset($_SESSION['logged'])) :?>
+<?php $user = R::findOne('users' , 'id = ?', array($_COOKIE['id']));
+    $usera = R::findOne('insts' , 'uid = ?', array($_COOKIE['id']));
+    $active_insts = R::findAll('insts','uid = ? AND active="1"',[$_COOKIE['id']]);
+    $dis_insts = R::findAll('insts','uid = ? AND active!="1"',[$_COOKIE['id']]);
+    ?>
 <?php if($_GET['id']){
   $id = $_GET['id'];
   $theory = R::findOne('theory', 'id = ?', (array($id)));
@@ -10,11 +16,11 @@
   <div class="v-lesson">
     <div class="v-l-title">
       <span>
-        <?php echo $theory->title; ?>
+        <?php echo $theory->title;?>
       </span>
     </div>
     <div class="video">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $theory->video; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <iframe width="100%" height="100%" src="<?php echo $theory->video; ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
   </div>
 </div>
@@ -23,6 +29,5 @@
 </body>
 <script src="js/js.js"></script>
 <script src="js/brain.js"></script>
-
-
-
+<?php else : echo'<meta http-equiv="refresh" content="0; url=http://as">'; ?>
+<?php endif; ?>
