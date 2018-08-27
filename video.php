175ee49fa8491b3b6b7159560d7b77f44986e4be
Zhandos <saginov-2001@mail.ru>
2018-08-27 23:46:49 +0300

    rsfsa

---
+++ video.php
@@ -0,0 +1,38 @@
+<?php include 'header2.php';
+  $videos = R::findAll('theory');?>
+
+
+<div class="container nm">
+  <div class="courses">
+    <?php 
+      foreach ($videos as $video) {
+        if ((int)$video->id == 1) {
+          echo('<a href="/testdb.php?id='.$video->id.'" style="text-decoration: none; color: white">
+      <div class="course">
+        <span class="c-title">'.$video->title.'
+        </span>
+        <span class="free">Бесплатно</span>
+      </div>
+    </a>');
+        } else{
+          echo('<a href="/testdb.php?id='.$video->id.'" style="text-decoration: none; color: white">
+      <div class="course" style="opacity: 0.9;">
+        <span class="c-title">'.$video->title.'
+        </span>
+        <span class="paid">Платно</span>
+      </div>
+    </a>');
+        }
+      }
+     ?>
+    
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
+</body>
+<script src="js/js.js"></script>
+<script src="js/brain.js"></script>
+
+
+
